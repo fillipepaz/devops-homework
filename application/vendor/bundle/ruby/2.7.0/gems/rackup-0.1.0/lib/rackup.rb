1d9be818314a87dc8d87ed25fc4ae787c3b6a3aa
Fillipe <git@bitbucket.com>
2025-09-28 02:53:16 -0300

    remove build files

--- application/vendor/bundle/ruby/2.7.0/gems/rackup-0.1.0/lib/rackup.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'rackup/handler'
-require_relative 'rackup/server'
-require_relative 'rackup/version'
-
-require_relative 'rackup/handler/webrick'
-require_relative 'rackup/handler/cgi'
